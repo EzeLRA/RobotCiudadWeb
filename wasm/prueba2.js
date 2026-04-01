00663a582e77daeb0795639fc193dd865cb31848
EzeLRA <ezeleocraft@gmail.com>
2026-03-31 22:33:02 -0300

    general fix

--- wasm/prueba2.js
+++ wasm/prueba2.js
@@ -2080,7 +2080,7 @@ export { wasm as __wasm }
 globalThis.__wasm_split_main_initSync = initSync;
 
 // Actually perform the load
-__wbg_init({module_or_path: "/./wasm/prueba2_bg.wasm"}).then((wasm) => {
+__wbg_init({module_or_path: "/RobotCiudadWeb/wasm/prueba2_bg.wasm"}).then((wasm) => {
     // assign this module to be accessible globally
     globalThis.__dx_mainWasm = wasm;
     globalThis.__dx_mainInit = __wbg_init;
